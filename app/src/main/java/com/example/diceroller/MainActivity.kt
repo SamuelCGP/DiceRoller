a2425856af7b91ed5915859d727230e8efd424f2
SamuelCGP <69848773+SamuelCGP@users.noreply.github.com>
2022-03-21 18:37:30 -0300

    second commit
    bug fixes and general improvement

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,21 +5,37 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton = findViewById<TextView>(R.id.roll_button)
+        val rollButton = findViewById<Button>(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
     }
 
     private fun rollDice(){
         val randomInt = (1..6).random()
         val labelResult: TextView = findViewById(R.id.label_result)
+        val rollButton = findViewById<Button>(R.id.roll_button)
 
-        labelResult.setText(randomInt)
+        labelResult.text = randomInt.toString()
+
+        val buttonColor = when (randomInt){
+            1 -> "#FFF44336"
+            2 -> "#FFFF5722"
+            3 -> "#FFFFC107"
+            4 -> "#FF8BC34A"
+            5 -> "#FF3F51B5"
+            else -> "#FF673AB7"
+        }
+
+        rollButton.setBackgroundColor(Color.parseColor(buttonColor))
+        labelResult.setTextColor(Color.parseColor(buttonColor))
 
         Toast.makeText(this, "Result: $randomInt",
             Toast.LENGTH_SHORT).show()
